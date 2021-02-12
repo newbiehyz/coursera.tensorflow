b5f219e955e40ef387cbca129d9d5ef669c09e6e
newbiehyz <55365761+newbiehyz@users.noreply.github.com>
2021-02-12 20:43:19 +0100

    Update week1.py

--- week1.py
+++ week1.py
@@ -5,8 +5,8 @@ So, imagine if house pricing was as easy as a house costs 50k + 50k per bedroom,
 
 How would you create a neural network that learns this relationship so that it would predict a 7 bedroom house as costing close to 400k etc.
 
-Hint: Your network might work better if you scale the house price down. You don't have to give the answer 400...it might be better to create something that predicts the number 4
-, and then your answer is in the 'hundreds of thousands' etc.
+Hint: Your network might work better if you scale the house price down. You don't have to give the answer 400...
+it might be better to create something that predicts the number 4, and then your answer is in the 'hundreds of thousands' etc.
 """
 
 import tensorflow as tf
